8cd1e326e1325368452b3df32318d6f12bca0000
thế anh hoàng <hoangtheanh166@gmail.com>
2019-06-14 15:10:06 +0700

    l

--- face/application/views/templates/_parts/public_master_footer_view.php
+++
@@ -1,8 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
-<footer>
-    
-</footer>
-<script src="<?php echo site_url('assets/admin/js/bootstrap.min.js');?>"></script>
-<?php echo $before_body;?>
-</body>
-</html>
--- face/application/views/templates/_parts/user_menu_admin_view.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
-<li class="divider"></li>
-<li id='top_menu_groups'><a href="<?php echo site_url('admin/groups'); ?>"><?php echo $this->lang->line('groups'); ?></a></li>
-<li id='top_menu_users'><a href="<?php echo site_url('admin/users'); ?>"><?php echo $this->lang->line('users'); ?></a></li>
